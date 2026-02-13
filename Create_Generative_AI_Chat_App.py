e0d8caa5167f4455cb28a4aec16d8c0a40d73e37
vshlkumar746 <60897132+vshlkumar746@users.noreply.github.com>
2026-02-13 07:36:23 -0800

    Create generative AI chat application

---
+++ Create_Generative_AI_Chat_App.py
@@ -0,0 +1,68 @@
+import os
+from dotenv import load_dotenv
+
+# Add references
+from azure.identity import DefaultAzureCredential
+from azure.ai.projects import AIProjectClient
+from openai import AzureOpenAI
+
+
+def main(): 
+
+    # Clear the console
+    os.system('cls' if os.name=='nt' else 'clear')
+        
+    try: 
+    
+        # Get configuration settings 
+        load_dotenv()
+        project_endpoint = os.getenv("PROJECT_ENDPOINT")
+        model_deployment =  os.getenv("MODEL_DEPLOYMENT")
+
+        # Initialize the project client
+        project_client = AIProjectClient(            
+                    credential=DefaultAzureCredential(            
+                                exclude_environment_credential=True,
+                                exclude_managed_identity_credential=True
+                    ),
+                    endpoint=project_endpoint,
+        )
+        
+
+        # Get a chat client
+        openai_client = project_client.get_openai_client(api_version="2024-10-21")
+
+
+        # Initialize prompt with system message
+        prompt = [
+                    {"role": "system", "content": "You are a helpful AI assistant that answers questions."}
+        ]
+    
+         
+
+        # Loop until the user types 'quit'
+        while True:
+            # Get input text
+            input_text = input("Enter the prompt (or type 'quit' to exit): ")
+            if input_text.lower() == "quit":
+                break
+            if len(input_text) == 0:
+                print("Please enter a prompt.")
+                continue
+            
+            # Get a chat completion
+            prompt.append({"role": "user", "content": input_text})
+            response = openai_client.chat.completions.create(
+                        model=model_deployment,
+                        messages=prompt)
+            completion = response.choices[0].message.content
+            print(completion)
+            prompt.append({"role": "assistant", "content": completion})
+            
+
+
+    except Exception as ex:
+        print(ex)
+
+if __name__ == '__main__': 
+    main()
