2de58bac9bbe23ab0c94524d88bd3abddd48fc59
vshlkumar746 <60897132+vshlkumar746@users.noreply.github.com>
2026-02-17 04:48:14 -0800

    Add Azure AI Language setup instructions
    Added setup instructions for Azure AI Language resource and environment configuration.
    For example, suppose a travel agency wants to process hotel reviews that have been submitted to the company's web site. By using the Azure AI Language, they can determine the language each review is written in, the sentiment (positive, neutral, or negative) of the reviews, key phrases that might indicate the main topics discussed in the review, and named entities, such as places, landmarks, or people mentioned in the reviews. In this exercise, you'll use the Azure AI Language Python SDK for text analytics to implement a simple hotel review application based on this example.
    
    While this exercise is based on Python, you can develop text analytics applications using multiple language-specific SDKs

---
+++ Analyze Text_Lab11.py
@@ -0,0 +1,89 @@
+"""
+Provision an Azure AI Language resource
+take its key & endpoint
+
+rm -r mslearn-ai-language -f
+git clone https://github.com/microsoftlearning/mslearn-ai-language
+cd mslearn-ai-language/Labfiles/01-analyze-text/Python/text-analysis
+python -m venv labenv
+./labenv/bin/Activate.ps1
+pip install -r requirements.txt azure-ai-textanalytics==5.3.0
+
+In requirment.txt
+python-dotenv
+
+In.env
+AI_SERVICE_ENDPOINT=https://language59218033.cognitiveservices.azure.com/
+AI_SERVICE_KEY=3BYMsvtyB1ymHL0qjldDMCV21RZPnqUsgrpvYON2wT02RLhVe5bAJQQJ99CBACHrzpqXJ3w3AAAaACOGEL3T
+
+"""
+
+from dotenv import load_dotenv
+import os
+
+# import namespaces
+from azure.core.credentials import AzureKeyCredential
+from azure.ai.textanalytics import TextAnalyticsClient
+
+
+def main():
+    try:
+        # Get Configuration Settings
+        load_dotenv()
+        ai_endpoint = os.getenv('AI_SERVICE_ENDPOINT')
+        ai_key = os.getenv('AI_SERVICE_KEY')
+
+        # Create client using endpoint and key
+        credential = AzureKeyCredential(ai_key)
+        ai_client = TextAnalyticsClient(endpoint=ai_endpoint, credential=credential)
+
+
+        # Analyze each text file in the reviews folder
+        reviews_folder = 'reviews'
+        for file_name in os.listdir(reviews_folder):
+            # Read the file contents
+            print('\n-------------\n' + file_name)
+            text = open(os.path.join(reviews_folder, file_name), encoding='utf8').read()
+            print('\n' + text)
+
+            # Get language
+            detectedLanguage = ai_client.detect_language(documents=[text])[0]
+            print('\nLanguage: {}'.format(detectedLanguage.primary_language.name))
+
+
+            # Get sentiment
+            sentimentAnalysis = ai_client.analyze_sentiment(documents=[text])[0]
+            print("\nSentiment: {}".format(sentimentAnalysis.sentiment))
+
+
+            # Get key phrases
+            phrases = ai_client.extract_key_phrases(documents=[text])[0].key_phrases
+            if len(phrases) > 0:
+                print("\nKey Phrases:")
+                for phrase in phrases:
+                    print('\t{}'.format(phrase))
+
+
+            # Get entities
+            entities = ai_client.recognize_entities(documents=[text])[0].entities
+            if len(entities) > 0:
+                print("\nEntities")
+                for entity in entities:
+                    print('\t{} ({})'.format(entity.text, entity.category))
+
+
+            # Get linked entities
+            entities = ai_client.recognize_linked_entities(documents=[text])[0].entities
+            if len(entities) > 0:
+                print("\nLinks")
+                for linked_entity in entities:
+                    print('\t{} ({})'.format(linked_entity.name, linked_entity.url))
+
+
+
+    except Exception as ex:
+        print(ex)
+
+
+if __name__ == "__main__":
+    main()
